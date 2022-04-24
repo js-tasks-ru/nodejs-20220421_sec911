26111b2db3bf71cd6276a22a7cfd05edcf195ceb
Sobolevskaya <evgeniya.sobolevskaya@refinitiv.com>
2022-04-24 18:04:56 +0200

    task_1: add sum function

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+  const message = 'Pay attention, a and b should be a number';
+  const type = 'number';
+
+  if (typeof a === type && typeof b === type) {
+    return a + b;
+  } else {
+    throw new TypeError(message);
+  }
 }
 
 module.exports = sum;
